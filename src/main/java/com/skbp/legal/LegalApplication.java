4be9a991fe02c5168c4de43f0d9950111bfbecf3
wooyoung85 <cerealtigerpower@gmail.com>
2020-02-18 17:42:21 +0900

    first commit

---
+++ src/main/java/com/skbp/legal/LegalApplication.java
@@ -0,0 +1,13 @@
+package com.skbp.legal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skbp/legal/LegalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skbp.legal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
